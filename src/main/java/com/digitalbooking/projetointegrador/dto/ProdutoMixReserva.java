e5d95c984b5b4c9fd028584569cffaba8813f7e8
jardel978 <jardeloliver197@gmail.com>
2022-04-02 14:35:18 -0300

    terceiro commit -produtoMixReservaDTO

--- src/main/java/com/digitalbooking/projetointegrador/dto/ProdutoMixReserva.java
+++ src/main/java/com/digitalbooking/projetointegrador/dto/ProdutoMixReserva.java
@@ -1,7 +1,14 @@
 package com.digitalbooking.projetointegrador.dto;
 
+import lombok.*;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProdutoMixReserva implements Serializable {
 
     private static final long serialVersionUID = 1L;
