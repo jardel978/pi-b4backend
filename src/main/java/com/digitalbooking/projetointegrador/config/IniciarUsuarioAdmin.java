6d679fc4ee953a252be2177164d54a7fdf83963c
Jardel Oliveira <jardeloliver197@gmail.com>
2022-04-04 15:48:50 -0300

    Update IniciarUsuarioAdmin.java

--- src/main/java/com/digitalbooking/projetointegrador/config/IniciarUsuarioAdmin.java
+++ src/main/java/com/digitalbooking/projetointegrador/config/IniciarUsuarioAdmin.java
@@ -7,6 +7,7 @@ import com.digitalbooking.projetointegrador.model.Funcao;
 import com.digitalbooking.projetointegrador.model.Usuario;
 import com.digitalbooking.projetointegrador.model.enums.NomeFuncao;
 import com.digitalbooking.projetointegrador.repository.IFuncaoRepository;
+import com.digitalbooking.projetointegrador.repository.IUsuarioRepository;
 import com.digitalbooking.projetointegrador.service.ClienteService;
 import com.digitalbooking.projetointegrador.service.UsuarioService;
 import org.modelmapper.ModelMapper;
@@ -26,6 +27,9 @@ public class IniciarUsuarioAdmin implements CommandLineRunner {
 
    @Autowired
    private IFuncaoRepository funcaoRepository;
+   
+   @Autowired
+   private IUsuarioRepository usuarioRepository;
 
    @Autowired
    private PasswordEncoder passwordEncoder;
@@ -37,21 +41,21 @@ public class IniciarUsuarioAdmin implements CommandLineRunner {
    public void run(String... args) throws Exception {
        try {
 
-           Funcao funcaoAdmin = new Funcao();
-           funcaoAdmin.setNomeFuncaoEnum(NomeFuncao.ADMIN);
-           Funcao funcaoAdminSalva = funcaoRepository.saveAndFlush(funcaoAdmin);
+//            Funcao funcaoAdmin = new Funcao();
+//            funcaoAdmin.setNomeFuncaoEnum(NomeFuncao.ADMIN);
+//            Funcao funcaoAdminSalva = funcaoRepository.saveAndFlush(funcaoAdmin);
 
-           Funcao funcaoUser = new Funcao();
-         funcaoUser.setId(1);
-           funcaoUser.setNomeFuncaoEnum(NomeFuncao.USER);
-           Funcao funcaoUserSalva = funcaoRepository.saveAndFlush(funcaoUser);
+//            Funcao funcaoUser = new Funcao();
+//          funcaoUser.setId(1);
+//            funcaoUser.setNomeFuncaoEnum(NomeFuncao.USER);
+//            Funcao funcaoUserSalva = funcaoRepository.saveAndFlush(funcaoUser);
 
            Usuario usuarioAdmin = new Usuario();
            usuarioAdmin.setNome("Admin");
            usuarioAdmin.setSobrenome("Admin");
            usuarioAdmin.setEmail("admindb@gmail.com");
            usuarioAdmin.setSenha("admin123");
-           usuarioAdmin.setFuncao(funcaoUser);
+//            usuarioAdmin.setFuncao(funcaoUser);
 
 //            Cliente usuarioComum = new Cliente();
 //            usuarioComum.setNome("Cliente");
@@ -61,7 +65,8 @@ public class IniciarUsuarioAdmin implements CommandLineRunner {
 //            usuarioComum.setSenha("cliente123");
 //            usuarioComum.setEndereco("Rua Luiz Boali N100 - Floresta, Itaipé/MG");
 
-           usuarioService.salvar(modelMapper.map(usuarioAdmin, UsuarioDTO.class));
+           usuarioRepository.save(usuarioAdmin);
+//            usuarioService.salvar(modelMapper.map(usuarioAdmin, UsuarioDTO.class));
 //            clienteService.salvar(modelMapper.map(usuarioComum, ClienteDTO.class));
 
 
