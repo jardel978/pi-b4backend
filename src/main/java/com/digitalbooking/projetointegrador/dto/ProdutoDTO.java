bdf0635726e189c42b83994e6e7a3088c8107dd5
Jardel Oliveira <jardeloliver197@gmail.com>
2022-04-18 19:44:03 -0300

    Update ProdutoDTO.java

--- src/main/java/com/digitalbooking/projetointegrador/dto/ProdutoDTO.java
+++ src/main/java/com/digitalbooking/projetointegrador/dto/ProdutoDTO.java
@@ -19,6 +19,7 @@ public class ProdutoDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+   
     private Long id;
 
     @NotNull(message = "O nome do produto não pode ser nulo")
@@ -27,11 +28,29 @@ public class ProdutoDTO implements Serializable {
     @Size(max = 3000, message = "A descrição informada excede a quantidade máxima de 3000 caracteres")
     private String descricao;
 
+    @DecimalMin(value = "0.0", message = "O valor da nota de avaliação não pode ser menor que 0")
+    @DecimalMax(value = "10.0", message = "O valor da nota de avaliação não pode ser maior que 10.0")
+    private Double notaDeAvaliacao;
+
+    @DecimalMin(value = "0.0", message = "O valor de reserva por pessoa não pode ser menor que 0")
+    private Double valorPorPessoa;
+
+    private int qtdAvaliacoes;
+
     @Size(max = 5, message = "A quantidade máxima de 5 imagens para esse produto foi excedida")
     private Set<ImagemDTO> imagens;
 
+    @NotNull(message = "O endereço deve ser informado")
+    private String endereco;
+
     @NotNull(message = "A cidade é um campo obrigatório")
     private CidadeDTO cidade;
+
+    @NotNull(message = "A latitude deve ser informada")
+    private Double latitude;
+
+    @NotNull(message = "A longitude deve ser informada")
+    private Double longitude;
 
     @NotNull(message = "A categoria do produto deve ser informada")
     private CategoriaDTO categoria;
