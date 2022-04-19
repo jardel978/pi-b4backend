e0b4e601e9a785d677d122cd095677b94b06c9a7
Jardel Oliveira <jardeloliver197@gmail.com>
2022-04-18 21:16:23 -0300

    Update Produto.java

--- src/main/java/com/digitalbooking/projetointegrador/model/Produto.java
+++ src/main/java/com/digitalbooking/projetointegrador/model/Produto.java
@@ -32,7 +32,7 @@ public class Produto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Id
+      @Id
     @Column(name = "produto_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -40,23 +40,43 @@ public class Produto implements Serializable {
     @Column(unique = true)
     private String nome;
 
+    @Column(length = 3000)
     private String descricao;
+
+    @Column(name = "nota_avaliacao_clientes")
+    private Double notaDeAvaliacao;
+
+    @Column(name = "valor_por_pessoa")
+    private Double valorPorPessoa;
+
+    @Column(name = "qtd_avaliacoes")
+    private int qtdAvaliacoes;
 
     @JsonIgnore
     @OneToMany(mappedBy = "produto", orphanRemoval = true)
     private Set<Imagem> imagens = new HashSet<>();
 
+    @Column(unique = true)
+    private String endereco;
+
     @ManyToOne
     @JoinColumn(name = "cidade_id", foreignKey = @ForeignKey(name = "fk_cidade_produto"))
     private Cidade cidade;
+
+    private Double latitude;
+
+    private Double longitude;
 
     @ManyToOne//muitos produtos para uma categoria
     @JoinColumn(name = "categoria_id", foreignKey = @ForeignKey(name = "fk_categoria_produto"))
     private Categoria categoria;
 
+    @OneToMany(mappedBy = "id.produto")
+    private Set<Score> scores = new HashSet<>();
+
     @ManyToMany
     @JoinTable(
-            name = "caracteristica_produto",
+            name = "tb_caracteristica_produto",
             joinColumns = @JoinColumn(name = "produto_id"),
             inverseJoinColumns = @JoinColumn(name = "caracteristica_id")
     )
@@ -71,5 +91,6 @@ public class Produto implements Serializable {
 
     @Transient//indica que esse campo não será persistido
     private List<LocalDate> datasIndisponiveis = new ArrayList<>();
+
 
 }
