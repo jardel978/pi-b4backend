a3edb61f9886e8c6be051b0a17dbb177bf13cf2c
jardel978 <jardeloliver197@gmail.com>
2022-04-27 23:38:54 -0300

    comentando arquivo

--- src/main/java/com/digitalbooking/projetointegrador/config/IniciarUsuarioAdmin.java
+++ src/main/java/com/digitalbooking/projetointegrador/config/IniciarUsuarioAdmin.java
@@ -1,70 +1,70 @@
-package com.digitalbooking.projetointegrador.config;
-
-import com.digitalbooking.projetointegrador.dto.ClienteDTO;
-import com.digitalbooking.projetointegrador.dto.UsuarioDTO;
-import com.digitalbooking.projetointegrador.model.Cliente;
-import com.digitalbooking.projetointegrador.model.Funcao;
-import com.digitalbooking.projetointegrador.model.Usuario;
-import com.digitalbooking.projetointegrador.model.enums.NomeFuncao;
-import com.digitalbooking.projetointegrador.repository.IFuncaoRepository;
-import com.digitalbooking.projetointegrador.service.ClienteService;
-import com.digitalbooking.projetointegrador.service.UsuarioService;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration//classe que inicia um usuario ao subir a aplicação (para testes)
-public class IniciarUsuarioAdmin implements CommandLineRunner {
-
-    @Autowired
-    private UsuarioService usuarioService;
-
-    @Autowired
-    private ClienteService clienteService;
-
-    @Autowired
-    private IFuncaoRepository funcaoRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private ModelMapper modelMapper;
-
-    @Override
-    public void run(String... args) throws Exception {
-        try {
-
-            Funcao funcaoAdmin = new Funcao();
-            funcaoAdmin.setNomeFuncaoEnum(NomeFuncao.ADMIN);
-            Funcao funcaoAdminSalva = funcaoRepository.saveAndFlush(funcaoAdmin);
-
-            Funcao funcaoUser = new Funcao();
-            funcaoUser.setNomeFuncaoEnum(NomeFuncao.USER);
-            Funcao funcaoUserSalva = funcaoRepository.saveAndFlush(funcaoUser);
-
-            Usuario usuarioAdmin = new Usuario();
-            usuarioAdmin.setNome("Jardel");
-            usuarioAdmin.setSobrenome("Oliveira");
-            usuarioAdmin.setEmail("jardeloliveiracontact@gmail.com");
-            usuarioAdmin.setSenha("admin123");
-            usuarioAdmin.setFuncao(funcaoAdminSalva);
-
-            Cliente usuarioComum = new Cliente();
-            usuarioComum.setNome("Cliente");
-            usuarioComum.setSobrenome("Cliente");
-            usuarioComum.setEmail("jardelsilvaoli@outlook.com");
-            usuarioComum.setFuncao(funcaoUserSalva);
-            usuarioComum.setSenha("cliente123");
-
-            usuarioService.salvar(modelMapper.map(usuarioAdmin, UsuarioDTO.class));
-            clienteService.salvar(modelMapper.map(usuarioComum, ClienteDTO.class));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-}
+//package com.digitalbooking.projetointegrador.config;
+//
+//import com.digitalbooking.projetointegrador.dto.ClienteDTO;
+//import com.digitalbooking.projetointegrador.dto.UsuarioDTO;
+//import com.digitalbooking.projetointegrador.model.Cliente;
+//import com.digitalbooking.projetointegrador.model.Funcao;
+//import com.digitalbooking.projetointegrador.model.Usuario;
+//import com.digitalbooking.projetointegrador.model.enums.NomeFuncao;
+//import com.digitalbooking.projetointegrador.repository.IFuncaoRepository;
+//import com.digitalbooking.projetointegrador.service.ClienteService;
+//import com.digitalbooking.projetointegrador.service.UsuarioService;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration//classe que inicia um usuario ao subir a aplicação (para testes)
+//public class IniciarUsuarioAdmin implements CommandLineRunner {
+//
+//    @Autowired
+//    private UsuarioService usuarioService;
+//
+//    @Autowired
+//    private ClienteService clienteService;
+//
+//    @Autowired
+//    private IFuncaoRepository funcaoRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    private ModelMapper modelMapper;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        try {
+//
+//            Funcao funcaoAdmin = new Funcao();
+//            funcaoAdmin.setNomeFuncaoEnum(NomeFuncao.ADMIN);
+//            Funcao funcaoAdminSalva = funcaoRepository.saveAndFlush(funcaoAdmin);
+//
+//            Funcao funcaoUser = new Funcao();
+//            funcaoUser.setNomeFuncaoEnum(NomeFuncao.USER);
+//            Funcao funcaoUserSalva = funcaoRepository.saveAndFlush(funcaoUser);
+//
+//            Usuario usuarioAdmin = new Usuario();
+//            usuarioAdmin.setNome("Jardel");
+//            usuarioAdmin.setSobrenome("Oliveira");
+//            usuarioAdmin.setEmail("jardeloliveiracontact@gmail.com");
+//            usuarioAdmin.setSenha("admin123");
+//            usuarioAdmin.setFuncao(funcaoAdminSalva);
+//
+//            Cliente usuarioComum = new Cliente();
+//            usuarioComum.setNome("Cliente");
+//            usuarioComum.setSobrenome("Cliente");
+//            usuarioComum.setEmail("jardelsilvaoli@outlook.com");
+//            usuarioComum.setFuncao(funcaoUserSalva);
+//            usuarioComum.setSenha("cliente123");
+//
+//            usuarioService.salvar(modelMapper.map(usuarioAdmin, UsuarioDTO.class));
+//            clienteService.salvar(modelMapper.map(usuarioComum, ClienteDTO.class));
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//}
